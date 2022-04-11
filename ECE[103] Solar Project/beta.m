55fbaa7fe1544995addd047ada9fee4b3238719d
colorfulparadox <101310062+colorfulparadox@users.noreply.github.com>
2022-04-10 20:43:14 -0700

    Delete ECE[103] Solar Project directory

--- ECE[103] Solar Project/beta.m
+++
@@ -1,7 +0,0 @@
-function [outputBeta] = beta(l, delta, h)
-lr = deg2rad(l);
-deltar = deg2rad(delta);
-hr = deg2rad(h);
-outputBeta = rad2deg(asin(cos(lr) * cos(deltar) * cos(hr) + sin(lr) * sin(deltar)));
-end
-
--- ECE[103] Solar Project/ed.m
+++
@@ -1,8 +0,0 @@
-function [ed] = ed(ptArray)
-num = 0;
-for output = ptArray
-    num = num + output;
-end
-ed = num;
-end
-
